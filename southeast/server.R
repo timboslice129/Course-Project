92737cf0d91448937e49fd5a0dc2fa576a8988fc
timboslice129 <tim.english365@gmail.com>
2021-04-29 15:34:23 +1000

    upload

--- southeast/server.R
+++ southeast/server.R
@@ -17,23 +17,23 @@ shinyServer(function(input, output) {
        
        while (i <= input$SIMS) {
            # current records
-           Atlanta <- 30
-           Miami <- 28
+           Atlanta <- 34
+           Miami <- 33
            
-           Atlantawinprobs <- c(input$IND,input$ORL,input$NYK,input$MIL, input$DET, input$PHI,input$PHI2, input$CHI, input$POR, 
+           Atlantawinprobs <- c(input$PHI2, input$CHI, input$POR, 
                                input$PHO, input$IND2, input$WAS, input$WAS2, input$ORL2, input$HOU)
            
            Atlantagameresults <- rbinom(length(Atlantawinprobs),1,Atlantawinprobs)
            
-           Miamiwinprobs <- c(input$tBKN, input$tSAS, input$tCHI, input$tCHI2, input$tSAS2, input$tCLE, input$tCHA, input$tDAL, input$tMIN,
+           Miamiwinprobs <- c(input$tCLE, input$tCHA, input$tDAL, input$tMIN,
                                 input$tBOS, input$tBOS2, input$tPHI, input$tMIL, input$tDET)
            
            
            Miamigameresults <- rbinom(length(Miamiwinprobs),1,Miamiwinprobs)
-           clash <- rbinom(1,1,input$MIA)
+          
            
-           Atlanta <- Atlanta + sum(Atlantagameresults) + clash
-           Miami <- Miami + sum(Miamigameresults) + abs(clash-1)
+           Atlanta <- Atlanta + sum(Atlantagameresults)
+           Miami <- Miami + sum(Miamigameresults) 
            
            b[i] = Atlanta
            t[i] = Miami
